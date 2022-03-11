0f1b9fd8346af14f50a445a02952f4469f80ef81
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-11 20:04:40 +0100

    Refactor attributes into a separate class

---
+++ src/models/Attributes.ts
@@ -0,0 +1,19 @@
+//T is interface like USerProps, K is one of keys in that interface, T[K] is type assigned to each key
+
+// interface UserProps {
+//   name: string;
+//   age: number;
+//   id: number;
+// }
+
+export class Attributes<T> {
+  constructor(private data: T) {}
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+// const attrs = new Attributes<UserProps>({name: 'efserg', age: 12, id: 1})
+// attrs.get('name')
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -10,12 +11,8 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(private data: UserProps) {}
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  public attributes: Attributes<UserProps>;
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
