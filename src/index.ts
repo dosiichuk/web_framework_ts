5092f8a8bad451a6746d6f55d57887bc6972ebc0
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-23 19:31:45 +0100

    Add function getters to User

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@ import { User } from './models/User';
 import axios from 'axios';
 
 const user = new User({ name: 'myname', age: 20 });
-user.set({ id: 1, name: 'XXX', age: 1000 });
-user.get('name');
-user.on('change', () => {});
 
 // axios.post('http://localhost:3000/users', {
 //   name: 'myname',
 //   age: 21,
 // });
 
-user.save();
+user.get('name');
